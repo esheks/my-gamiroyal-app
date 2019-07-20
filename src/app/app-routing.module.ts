2d7c8e9d457a648684d93a50c30099317e1af0ea
esheks <44941166+esheks@users.noreply.github.com>
2019-07-19 21:55:54 -0600

    first comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { SofaComponent } from './sofa/sofa.component';
+import { BedsComponent } from './beds/beds.component';
+import { DiningComponent } from './dining/dining.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'products', component: ProductsComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'sofa', component: SofaComponent},
+  {path:'beds', component: BedsComponent},
+  {path:'dining', component: DiningComponent},
+  {path:'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { SofaComponent } from './sofa/sofa.component';
+import { BedsComponent } from './beds/beds.component';
+import { DiningComponent } from './dining/dining.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    SofaComponent,
+    BedsComponent,
+    DiningComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
